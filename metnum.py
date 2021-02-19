4e330fdb6e29a08ec8582c3b929a88e3c6285c35
OmarPizano <oj.pizanoorozco@ugto.mx>
2021-02-19 07:03:10 -0600

    Actualizar método de falsa posición.

--- metnum.py
+++ metnum.py
@@ -57,24 +57,62 @@ def bisection(function, lower_x, upper_x, real_root = False, error_limit = 0.5, 
     # regresar la raíz encontrada
     return root
 
-def falsepos(f, xl, xu, xr, err, err_a, i, imax, rr):
-    xrold = xr
-    xr = xu - (f(xu) * (xl - xu) / (f(xl) - f(xu)) )
-    i += 1
-    if xr != 0:
-        err_a = abs((xr - xrold) / xr) * 100
-    err_t = abs(rr - xr)
-    print("{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(i,xl,xu,xr,err_a,err_t))
-    test = f(xl) * f(xr)
-    if test < 0:
-        xu = xr
-    elif test > 0:
-        xl = xr
-    else:
-        err_a = 0
-    if err_a > err and i < imax:
-        xr = bisection(f,xl,xu,xr,err,err_a,i,imax, rr)
-    return xr
+def falsepos(function, lower_x, upper_x, real_root = False, error_limit = 0.5, max_iteration = 20, table = False, summary = False):
+    tableline = 55      # tamaño de las líneas de la tabla
+    iteration = 0       # contador de iteración
+    root = 0            # raíz inicial
+    table_data = []     # matriz para los datos de la tabla
+    # resumen de los datos de entrada
+    if summary:
+        summ = "\nFunction: " + str(getsourcelines(function)[0][0][:-1].split(":")[1][1:])
+        summ += "\nLower X: " + str(lower_x)
+        summ += "\nUpper X: " + str(upper_x)
+        summ += "\nReal Root: " + str(real_root)
+        summ += "\nError Limit: " + str(error_limit)
+        summ += "\nMax Iteration: " + str(max_iteration)
+        summ += "\n"
+        print(summ)
+    # algoritmo de FALSA POSICIÓN
+    while True:
+        current_data = []   # lista para los datos de la iteración actual
+        old_root = root
+        root = upper_x - (function(upper_x) * (lower_x - upper_x)) / (function(lower_x) - function(upper_x))
+        iteration += 1
+        current_data.extend([iteration, lower_x, upper_x, root, old_root])
+        if root != 0:
+            relative_error = abs((root - old_root) / root ) * 100
+            current_data.append(relative_error)
+        test = function(lower_x) *  function(root)
+        if test < 0:
+            upper_x = root
+        elif test > 0:
+            lower_x = root
+        else:
+            relative_error = 0
+        # si se especifica una raíz real, se calcula el error real
+        if real_root:
+            real_error = abs((real_root - root) / real_root) * 100
+        else:
+            real_error = "---"
+        current_data.append(real_error)
+        # en este punto, ya se calcularon todos los datos de esta iteración, por lo que se añaden
+        table_data.append(current_data)
+        # condición de parada
+        if relative_error < error_limit or iteration >= max_iteration:
+            break
+    # tabla con los datos de cada iteración
+    if table:
+        print("ITER\tXL\tXU\tR\tOLD_R\tERR_A\tERR_R\n" + "-" * tableline)
+        for row in table_data:
+            if real_root:
+                print("{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t".format(\
+                        row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+            else:
+                print("{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}\t{}\t".format(\
+                        row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
+        print("-" * tableline)
+    # regresar la raíz encontrada
+    return root
 
 def fixpt(f, x0, err, imax, rr, verbose):
     i = 0
