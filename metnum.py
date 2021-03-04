763a0d3c1693b33fbc7f4447bd37c881b45365e3
OmarPizano <oj.pizanoorozco@ugto.mx>
2021-03-03 19:20:34 -0600

    Habilitar y mejorar la función 'newton_raphson' para el método de Newton-Raphson.

--- metnum.py
+++ metnum.py
@@ -161,21 +161,50 @@ def fixpt(function, init_x, real_root, error_limit = 0.5, max_iteration = 20, su
     # regresar la raíz encontrada
     return root
 
-# df = derivada de f.
-#def newton_raphson(f, df, x0, err, imax, rr, verbose):
-#    i = 0
-#    xr = x0
-#    if verbose:
-#        print("N°\tx0\txr\terr_a\terr_t\n"+"-"*50)
-#    while True:
-#        xrold = xr
-#        xr = xrold - f(xr) / df(xr)
-#        if xr != 0:
-#            err_a = abs((xr - xrold) / xr) * 100
-#        err_t = abs(rr - xr)
-#        if verbose:
-#            print("{}\t{:.3f}\t{:.3f}\t{:.3f}\t{:.3f}".format(i,x0,xr,err_a,err_t))
-#        if err_a < err or i >= imax:
-#            break
-#        i += 1
-#    return xr
+def newton_raphson(function, derivative, init_x, real_root, error_limit = 0.5, max_iteration = 20, summary = False, table = False):
+    tableline = 55      # tamaño de las líneas de la tabla
+    iteration = 0       # contador de iteración
+    root = init_x       # raíz inicial
+    table_data = []     # matriz para los datos de la tabla
+    # resumen de los datos de entrada
+    if summary:
+        summ = "\nFunction: " + str(getsourcelines(function)[0][0][:-1].split(":")[1][1:])
+        summ = "\nDerivative: " + str(getsourcelines(derivative)[0][0][:-1].split(":")[1][1:])
+        summ += "\nInit X: " + str(init_x)
+        summ += "\nReal Root: " + str(real_root)
+        summ += "\nError Limit: " + str(error_limit)
+        summ += "\nMax Iteration: " + str(max_iteration)
+        summ += "\n"
+        print(summ)
+    while True:
+        current_data = []   # lista para los datos de la iteración actual
+        old_root = root
+        root = old_root - function(root) / derivative(root)
+        iteration += 1
+        current_data.extend([iteration, root])
+        if root != 0:
+            relative_error = abs((root - old_root) / root) * 100
+            current_data.append(relative_error)
+        # si se especifica una raíz real, se calcula el error real
+        if real_root:
+            real_error = abs((real_root - root) / real_root) * 100
+        else:
+            real_error = "---"
+        current_data.append(real_error)
+        # en este punto, ya se calcularon todos los datos de esta iteración, por lo que se añaden
+        table_data.append(current_data)
+        if relative_error < error_limit or iteration >= max_iteration:
+            break
+    # tabla con los datos de cada iteración
+    if table:
+        print("ITER\tXI\tERR_A\tERR_R\n" + "-" * tableline)
+        for row in table_data:
+            if real_root:
+                print("{}\t{:.3f}\t{:.3f}\t{:.3f}\t".format(\
+                        row[0], row[1], row[2], row[3]))
+            else:
+                print("{}\t{:.3f}\t{:.3f}\t{}\t".format(\
+                        row[0], row[1], row[2], row[3]))
+        print("-" * tableline)
+    # regresar la raíz encontrada
+    return root
